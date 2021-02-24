9caf56bfe48f5920addbefed20b070bade777acc
Johnny Delgado <johnny.m.delgado12@gmail.com>
2021-02-23 19:13:53 -0600

    Update helloWorld.java
    Finished syntax, and data type exercises

--- src/helloWorld.java
+++ src/helloWorld.java
@@ -1,5 +1,20 @@
 public class helloWorld {
+    
     public static void main(String[] args) {
-        System.out.println("Hello, World");
+        System.out.println("I once said \"I love cats!\"");
+        int myFavoriteNumber = 20;
+        System.out.println(myFavoriteNumber);
+        String myString = "I love Cats!";
+        System.out.println(myString);
+        double myNumber = 3.14;
+        System.out.println(myNumber);
+        int x = 5;
+        System.out.println(++x);
+        System.out.println(x);
+
+        String theNumberThree = "three";
+        Object o = theNumberThree;
+        System.out.println(o);
+
     }
 }
