4ca58be749f0e914a2e9246799ded4d10f02c6e6
Johnny Delgado <johnny.m.delgado12@gmail.com>
2021-02-24 20:54:32 -0600

    Update controlFlowExercises.java
    completed fizz buzz exercise

--- src/controlFlowExercises.java
+++ src/controlFlowExercises.java
@@ -21,5 +21,15 @@ public class controlFlowExercises {
         for(i = 2; i <= 1000000; i *= i){
             System.out.println(i);
         }
+
+        for(i = 0; i <=100; i++){
+            if(i % 15 == 0){
+                System.out.println("Fizz Buzz");
+            }else if(i % 3 == 0){
+                System.out.println("Fizz");
+            }else if(i % 5 == 0){
+                System.out.println("Buzz");
+            }
+        }
     }
 }
